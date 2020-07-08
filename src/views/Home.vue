64fff32bfdc8ec423e4dc7548fd33460a5d57e3e
Wouter de Bruijn <git@electrogamez.nl>
2020-07-08 17:50:03 +0200

    added vue examples for learning

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,38 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="Home">
+    The home view
+    <HelloWorld msg="This is a prop! This gets data from the parent!" /><br>
+    <b>Same Hello world component but we use a variable this time:</b>
+    <HelloWorld :msg="foo" />
+    <div>
+      <button @click="upVariable">Click me!</button>
+    </div>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
 import HelloWorld from "@/components/HelloWorld.vue";
 
-export default {
-  name: "Home",
+@Component({
   components: {
     HelloWorld
   }
-};
+})
+export default class Home extends Vue {
+  private foo = 0;
+
+  mounted() {
+    console.log("This runs when this class is activated!");
+    this.upVariable();
+  }
+
+  upVariable() {
+    // This is a function that can be run via mounted or other events.
+    console.log("SetVariable function is run!");
+    this.foo++;
+  }
+}
 </script>
+
+<style scoped></style>
